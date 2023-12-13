b24890482b47e2fcc86d15e27e81cc21c1324059
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2023-12-12 23:18:53 -0500

    chore: typo

--- lua/kitty-scrollback/launch.lua
+++ lua/kitty-scrollback/launch.lua
@@ -216,7 +216,7 @@ local set_cursor_position = vim.schedule_wrap(function(d)
   local scrolled_by = d.scrolled_by
   local lines = d.lines - tab_offset
   if y < 0 then
-    -- TOOD: hack? not sure but works
+    -- TODO: hack? not sure but works
     lines = lines + math.abs(y)
     y = 0
   end
