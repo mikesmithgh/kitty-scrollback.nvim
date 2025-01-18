f971d0b3705f7027a18c6ce70e7f2d091ff0f0b0
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2025-01-17 20:16:11 -0500

    chore: update branch protection script base dir

--- scripts/internal/__update_branch_protection_rules.sh
+++ scripts/internal/__update_branch_protection_rules.sh
@@ -3,4 +3,6 @@
 # update branch protection rules via the github API
 # the test matrix can get complex, this offers a convenient way to update the branch protection rules instead of clicking
 # each individual action in the UI
-gh api /repos/mikesmithgh/kitty-scrollback.nvim/branches/main/protection --method PUT --input __branch-protection-rules.json
+
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+gh api /repos/mikesmithgh/kitty-scrollback.nvim/branches/main/protection --method PUT --input "$script_dir/__branch-protection-rules.json"
